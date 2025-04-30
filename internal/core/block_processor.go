f5496a6a813a7c1024ca2f67842635c88b749456
samuel0642 <samuel.goodman@crossdag.com>
2025-04-30 05:16:24 +0000

    log: tx log from mempool

--- internal/core/block_processor.go
+++ internal/core/block_processor.go
@@ -3,24 +3,77 @@ package core
 import (
 	"time"
 	"sync"
+	"crypto/sha256"
+	"encoding/json"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"log"
 
 	"github.com/CrossDAG/BlazeDAG/internal/types"
 )
 
+// Mempool stores pending transactions
+type Mempool struct {
+	transactions map[string]*types.Transaction
+	mu           sync.RWMutex
+}
+
+// NewMempool creates a new mempool
+func NewMempool() *Mempool {
+	return &Mempool{
+		transactions: make(map[string]*types.Transaction),
+	}
+}
+
+// AddTransaction adds a transaction to the mempool
+func (mp *Mempool) AddTransaction(tx *types.Transaction) {
+	mp.mu.Lock()
+	defer mp.mu.Unlock()
+	mp.transactions[string(tx.From)] = tx
+	log.Printf("Added transaction to mempool - From: %x, To: %x, Value: %d, Nonce: %d", 
+		tx.From, tx.To, tx.Value, tx.Nonce)
+}
+
+// GetTransactions returns all transactions in the mempool
+func (mp *Mempool) GetTransactions() []*types.Transaction {
+	mp.mu.RLock()
+	defer mp.mu.RUnlock()
+	txs := make([]*types.Transaction, 0, len(mp.transactions))
+	for _, tx := range mp.transactions {
+		txs = append(txs, tx)
+	}
+	log.Printf("Retrieved %d transactions from mempool", len(txs))
+	return txs
+}
+
+// RemoveTransactions removes transactions from the mempool
+func (mp *Mempool) RemoveTransactions(txs []*types.Transaction) {
+	mp.mu.Lock()
+	defer mp.mu.Unlock()
+	for _, tx := range txs {
+		delete(mp.transactions, string(tx.From))
+		log.Printf("Removed transaction from mempool - From: %x, To: %x, Value: %d, Nonce: %d", 
+			tx.From, tx.To, tx.Value, tx.Nonce)
+	}
+}
+
 // BlockProcessor handles block creation and certification
 type BlockProcessor struct {
-	config *Config
-	state  *State
-	dag    *DAG
-	mu     sync.RWMutex
+	config  *Config
+	state   *State
+	dag     *DAG
+	mempool *Mempool
+	mu      sync.RWMutex
 }
 
 // NewBlockProcessor creates a new block processor
 func NewBlockProcessor(config *Config, state *State, dag *DAG) *BlockProcessor {
 	return &BlockProcessor{
-		config: config,
-		state:  state,
-		dag:    dag,
+		config:  config,
+		state:   state,
+		dag:     dag,
+		mempool: NewMempool(),
 	}
 }
 
@@ -28,6 +81,10 @@ func NewBlockProcessor(config *Config, state *State, dag *DAG) *BlockProcessor {
 func (bp *BlockProcessor) CreateBlock(round types.Round) (*types.Block, error) {
 	bp.mu.Lock()
 	defer bp.mu.Unlock()
+
+	// Get transactions from mempool
+	txs := bp.mempool.GetTransactions()
+	log.Printf("Creating new block with %d transactions for round %d", len(txs), round)
 
 	// Create block header
 	header := &types.BlockHeader{
@@ -38,13 +95,13 @@ func (bp *BlockProcessor) CreateBlock(round types.Round) (*types.Block, error) {
 		Height:     types.BlockNumber(bp.getNextHeight()),
 		ParentHash: bp.getParentHash(),
 		References: bp.selectReferences(),
-		StateRoot:  bp.calculateStateRoot(),
+		StateRoot:  bp.calculateStateRoot(txs),
 		Validator:  bp.config.NodeID,
 	}
 
 	// Create block body
 	body := &types.BlockBody{
-		Transactions: bp.collectTransactions(),
+		Transactions: txs,
 		Receipts:     make([]*types.Receipt, 0),
 		Events:       make([]*types.Event, 0),
 	}
@@ -59,6 +116,10 @@ func (bp *BlockProcessor) CreateBlock(round types.Round) (*types.Block, error) {
 	if err := bp.signBlock(block); err != nil {
 		return nil, err
 	}
+
+	// Remove processed transactions from mempool
+	bp.mempool.RemoveTransactions(txs)
+	log.Printf("Block created successfully with %d transactions", len(txs))
 
 	return block, nil
 }
@@ -97,30 +158,73 @@ func (bp *BlockProcessor) selectReferences() []*types.Reference {
 	return references
 }
 
-// calculateStateRoot calculates the state root
-func (bp *BlockProcessor) calculateStateRoot() types.Hash {
-	// TODO: Implement state root calculation
-	// For now, return a dummy hash
-	return types.Hash{}
+// calculateStateRoot calculates the state root based on transactions
+func (bp *BlockProcessor) calculateStateRoot(txs []*types.Transaction) types.Hash {
+	// Create a map to store state changes
+	stateChanges := make(map[string]interface{})
+
+	// Process each transaction
+	for _, tx := range txs {
+		// For each transaction, update the state
+		// This is a simplified example - in a real implementation,
+		// you would have more complex state transition logic
+		stateChanges[string(tx.From)] = map[string]interface{}{
+			"balance": tx.Value,
+			"nonce":   tx.Nonce,
+		}
+	}
+
+	// Serialize state changes to JSON
+	stateBytes, err := json.Marshal(stateChanges)
+	if err != nil {
+		return types.Hash{}
+	}
+
+	// Calculate hash of state changes
+	hash := sha256.Sum256(stateBytes)
+	return types.Hash(hash[:])
 }
 
-// collectTransactions collects transactions for the new block
-func (bp *BlockProcessor) collectTransactions() []*types.Transaction {
-	// TODO: Implement transaction collection logic
-	// For now, return empty transactions
-	return make([]*types.Transaction, 0)
-}
-
-// signBlock signs the block
+// signBlock signs the block using ECDSA
 func (bp *BlockProcessor) signBlock(block *types.Block) error {
-	// TODO: Implement block signing
-	// For now, just set a dummy signature
+	// Serialize block header for signing
+	headerBytes, err := json.Marshal(block.Header)
+	if err != nil {
+		return err
+	}
+
+	// Create a new private key for signing
+	// In a real implementation, you would use the node's private key
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	// Hash the header bytes
+	hash := sha256.Sum256(headerBytes)
+
+	// Sign the hash
+	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hash[:])
+	if err != nil {
+		return err
+	}
+
+	// Convert signature to bytes
+	signature := append(r.Bytes(), s.Bytes()...)
+
+	// Set the signature in the block header
 	block.Header.Signature = types.Signature{
 		Validator:  bp.config.NodeID,
-		Signature:  []byte("dummy_signature"),
+		Signature:  signature,
 		Timestamp:  time.Now(),
 	}
+
 	return nil
+}
+
+// AddTransaction adds a transaction to the mempool
+func (bp *BlockProcessor) AddTransaction(tx *types.Transaction) {
+	bp.mempool.AddTransaction(tx)
 }
 
 // CreateCertificate creates a certificate for a block
